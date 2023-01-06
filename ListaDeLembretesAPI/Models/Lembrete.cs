ebc6f0e75d843cd2959823f1b932394374f4cb1b
Uiseman <uiseman@gmail.com>
2023-01-06 17:54:08 -0300

    adição de pacotes

--- ListaDeLembretesAPI/Models/Lembrete.cs
+++ ListaDeLembretesAPI/Models/Lembrete.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace ListaDeLembretesAPI.Models
 {
     public class Lembrete
     {
         public int LembreteId { get; set; }
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
         public DateOnly Data { get; set; }
 
     }
