7b80cac9c080e60e0f3b18ca4889f7f6b8876cd5
Uiseman <uiseman@gmail.com>
2023-01-06 21:22:09 -0300

    Endpoints Criados

--- ListaDeLembretesAPI/Controllers/LembretesController.cs
+++ ListaDeLembretesAPI/Controllers/LembretesController.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using ListaDeLembretesAPI.Context;
+using ListaDeLembretesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -12,6 +15,99 @@ namespace ListaDeLembretesAPI.Controllers
     [Route("[controller]")]
     public class LembretesController : Controller
     {
+        private readonly AppDbContext? _context;
 
+        public LembretesController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+
+        [HttpGet]
+
+        public ActionResult<IEnumerable<Lembrete>> Get()
+        {
+            try
+            {
+                var lembretes = _context.Lembretes.ToList();
+
+                if (lembretes is null)
+                {
+                    return NotFound();
+                }
+                return lembretes;
+            }
+
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Problema no tratamento da solicitação");
+            }
+
+        }
+
+        [HttpGet("{id:int}", Name = "ObterLembretePeloId")]
+        public ActionResult<Lembrete> Get(int id)
+        {
+            try
+            {
+                var lembrete = _context.Lembretes.FirstOrDefault(l => l.LembreteId == id);
+                if (lembrete is null)
+                {
+                    return NotFound("Lembrete não encontrado");
+                }
+                return lembrete;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Problema no tratamento da solicitação");
+            }
+
+        }
+
+        [HttpPost]
+
+        public ActionResult Post([FromBody] Lembrete lembrete)
+        {
+            try
+            {
+                _context.Lembretes.Add(lembrete);
+                _context.SaveChanges();
+                return new CreatedAtRouteResult("ObterLembretePeloId",
+                    new { id = lembrete.LembreteId }, lembrete);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Problema no tratamento da solicitação");
+            }
+
+        }
+
+        [HttpDelete("{id:int}")]
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var lembrete = _context.Lembretes.FirstOrDefault(l => l.LembreteId == id);
+                if (lembrete is null)
+                {
+                    return NotFound("Lembrete não localizado");
+                }
+                _context.Lembretes.Remove(lembrete);
+                _context.SaveChanges();
+
+                return Ok();
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Problema no tratamento da solicitação");
+
+            }
+        }
     }
 }
--- ListaDeLembretesAPI/Program.cs
+++ ListaDeLembretesAPI/Program.cs
@@ -3,10 +3,10 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -14,7 +14,6 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDataba
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
